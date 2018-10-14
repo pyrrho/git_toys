48985cb3881795158b7337c15b8740406b3fd678
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2018-10-14 16:17:26 -0400

    Update the format and contents of demo 003

--- 003_linearized_histories.sh
+++ 003_linearized_histories.sh
@@ -1,148 +1,154 @@
 #!/usr/bin/env bash
 set -euo pipefail
-IFS=$'\n\t'
+IFS=$'
+\t'
 
 source ./msgs.sh
 source ./gits.sh
 source ./operations.sh
 
-# This will start logging commands this script executes.
-set -x
-
-# This will stop logging, without printing the "stop logging commands" command.
-{ set +x; } 2>/dev/null
-
-title "This is going to build off of the previous demonstration, showing how a"\
-      "less-than-happy-path merge conflict might be resolved through"\
-      "linearization via git rebase."
+stitle 3<<EOM
+This is going to build off of the previous demonstration, showing how a
+less-than-happy-path merge conflict might be resolved through linearization
+via git rebase.
+EOM
 reset_repo
 
 msg "Files and branches incoming..."
 
-echo -e "Here is our first file" > file_a.txt
-{ set -x; } 2>/dev/null
-git add file_a.txt
-git commit -m "Add file_a.txt"
-{ set +x; } 2>/dev/null
+showcmd "Set up the history" 3<<'SCRIPT'
+    echo -e "Here is our first file" > file_a.txt
+    git add file_a.txt
+    git commit -m "Add file_a.txt"
 
-nl
-git_co -b new_files/1/file_b.txt
-echo -e "Here's another file." > file_b.txt
-{ set -x; } 2>/dev/null
-git add file_b.txt
-git commit -m "Add file_b.txt"
-{ set +x; } 2>/dev/null
+    git_co -b new_files/1/file_b.txt
+    echo -e "Here's another file." > file_b.txt
+    git add file_b.txt
+    git commit -m "Add file_b.txt"
 
-nl
-git_co -b new_files/2/file_c.txt
-echo -e "A third one...." > file_c.txt
-{ set -x; } 2>/dev/null
-git add file_c.txt
-git commit -m "Add file_c.txt"
-{ set +x; } 2>/dev/null
+    git_co -b new_files/2/file_c.txt
+    echo -e "A third one...." > file_c.txt
+    git add file_c.txt
+    git commit -m "Add file_c.txt"
 
-nl
-git_co -b new_files/3/file_d.txt
-echo -e "And a fourth, for good measure" > file_d.txt
-{ set -x; } 2>/dev/null
-git add file_d.txt
-git commit -m "Add file_d.txt"
-{ set +x; } 2>/dev/null
+    git_co -b new_files/3/file_d.txt
+    echo -e "And a fourth, for good measure" > file_d.txt
+    git add file_d.txt
+    git commit -m "Add file_d.txt"
+SCRIPT
 
-git_lg --all
+show_single_cmd git_lg --all
 
-wsg "If you've been following along, this setup will look very familiar."\
-    "\nWe're going to re-create the conflict we saw in the previous demo by"\
-    "editing file_b.txt in branches new_files/1 and new_files/2."
+swsg 3<<EOM
+If you've been following along, this setup will look very familiar.
 
-nl
-git_co new_files/1/file_b.txt
-describe_file file_b.txt
-nl
-{ set -x; } 2>/dev/null
-sed -i '' 's/another/an amazing/' file_b.txt
-{ set +x; } 2>/dev/null
-describe_file file_b.txt
+We're going to re-create the conflict we saw in the previous demo by editing
+file_b.txt in branches new_files/1 and new_files/2."
+EOM
 
-{ set -x; } 2>/dev/null
-git ci file_b.txt -m "Fixed a bug in file_b.txt"
-{ set +x; } 2>/dev/null
 
-nl
-git_co new_files/3/file_d.txt
-describe_file file_b.txt
-nl
-{ set -x; } 2>/dev/null
-sed -i '' 's/another/an important/' file_b.txt
-{ set +x; } 2>/dev/null
-describe_file file_b.txt
+showcmd "Check on file_b.txt in new_files/3..." 3<<'SCRIPT'
+    git_co new_files/3/file_d.txt
+    describe_file file_b.txt
+SCRIPT
+showcmd "... then edit and commit it" 3<<'SCRIPT'
+    sed -i '' 's/another/an important/' file_b.txt
+    describe_file file_b.txt
 
-{ set -x; } 2>/dev/null
-git ci file_b.txt -m "Squashed a bug in file_b.txt [Hey look! A conflict!]"
-{ set +x; } 2>/dev/null
+    git ci file_b.txt -m "Squashed a bug in file_b.txt [Hey look! A conflict!]"
+SCRIPT
 
-git_lg --all
+showcmd "Check on file_b.txt in new_files/1..." 3<<'SCRIPT'
+    git_co new_files/1/file_b.txt
+    describe_file file_b.txt
+SCRIPT
+showcmd "... then edit and commit it" 3<<'SCRIPT'
+    sed -i '' 's/another/an amazing/' file_b.txt
+    describe_file file_b.txt
 
-wsg "Here is the branch structure that eventually lead to a merge conflict in"\
-    "our prevous demo."\
-    "\nRather than waiting to find merge conflicts during the stack collapse,"\
-    "let's go looking for them by using git rebase to linearize the history of"\
-    "this stack."
+    git ci file_b.txt -m "Fixed a bug in file_b.txt"
+SCRIPT
 
-{ set -x; } 2>/dev/null
-git rebase new_files/1/file_b.txt new_files/2/file_c.txt --onto new_files/1/file_b.txt
-{ set +x; } 2>/dev/null
+show_single_cmd git_lg --all
 
-git_lg --all
+swsg 3<<EOM
+Here is the branch structure that eventually lead to a merge conflict in our
+prevous demo.
 
-wsg "The first rebase succeeded; new_files/2 is now on top of the branch that"\
-    "new_files/1 created."\
-    "\nLet's continue."
+Rather than waiting to find merge conflicts during the stack collapse, let's go
+looking for them by using git rebase to linearize the history of this stack.
+EOM
 
-{ set -x; } 2>/dev/null
+showcmd "Move new_files/2 onto the new new_files/1" 3<<'SCRIPT'
+    git rebase new_files/1/file_b.txt new_files/2/file_c.txt --onto new_files/1/file_b.txt
+SCRIPT
+
+show_single_cmd git_lg --all
+
+swsg 3<<EOM
+The first rebase succeeded; new_files/2 is now on top of the fork that
+new_files/1 created.
+
+new_files/3 is in it's own version of the history, though, no longer a
+descendent of new_files/2 or new_file/1.
+
+Let's continue.
+EOM
+
 { set +e; } 2>/dev/null
-git rebase new_files/2/file_c.txt new_files/3/file_d.txt --onto new_files/2/file_c.txt
+showcmd "Move new_file/3 onto the new new_files/2" 3<<'SCRIPT'
+    git rebase new_files/2/file_c.txt new_files/3/file_d.txt --onto new_files/2/file_c.txt
+SCRIPT
 { set -e; } 2>/dev/null
-{ set +x; } 2>/dev/null
 
+swsg 3<<EOM
+And here we are, back at a conflict. But this time it's in new_files/3 (or
+rather, a mid-rebase detached HEAD based on the changes made in new_files/3).
 
-wsg "And here we are, back at a conflict. But this time it's in new_files/3"\
-    "(or rather, a detached HEAD based on the changes made in new_files/3)."\
-    "\nLet's take a look at the state of the repository, make sure we know"\
-    "what's up..."
+Let's take a look at the state of the repository to make sure we know exactly
+what's going on.
+EOM
 
-git status
-nl
-describe_file file_b.txt
+show_single_cmd git status
+show_single_cmd describe_file file_b.txt
 
-wsg "Yep, that looks like a rebase conflict if I've ever seen one."\
-    "\nLet's fix that up by combining the two changes..."
+swsg 3<<EOM
+Yep, that looks like a rebase conflict if I've ever seen one.
 
-echo -e "Here's an important and amazing file." >| file_b.txt
-describe_file file_b.txt
-git add file_b.txt
-git rebase --continue
+Let's fix that up by combining the two changes...
+EOM
 
-git_lg --all
+showcmd "Fix the file_b.txt rebase conflict" 3<<'SCRIPT'
+    echo -e "Here's an important and amazing file." >| file_b.txt
+    describe_file file_b.txt
+    git add file_b.txt
+    git rebase --continue
+SCRIPT
 
-wsg "Now that the rebase conflict is resolved, we're back to a fully linear"\
-    "history. This merge is now guaranteed to complete without conflict."\
-    "We could even fast-forward merge master, but then we'd not know who"\
-    "performed the merge."
+show_single_cmd git_lg --all
 
-git_co new_files/2/file_c.txt
-git_merge_branch new_files/3/file_d.txt
-nl
+swsg 3<<EOM
+Now that the rebase conflict is resolved, we're back to a fully linear history.
+This merge is now guaranteed to complete without conflict. We could even
+fast-forward merge master, but then we'd not know who performed the merge.
+EOM
 
-git_co new_files/1/file_b.txt
-git_merge_branch new_files/2/file_c.txt
-nl
+showcmd "Collapse the stack" 3<<'SCRIPT'
+    git_co new_files/2/file_c.txt
+    git_merge_branch new_files/3/file_d.txt
 
-git_co master
-git_merge_branch new_files/1/file_b.txt
+    git_co new_files/1/file_b.txt
+    git_merge_branch new_files/2/file_c.txt
 
-git_lg --all
+    git_co master
+    git_merge_branch new_files/1/file_b.txt
+SCRIPT
 
-wsg "This concludes the first series of demos. Now that the first few pitfalls"\
-    "have been exposed, and the primary motivations have been shown it's time"\
-    "to dig into more corner cases and show off"
+show_single_cmd git_lg --all
+
+swsg 3<<EOM
+This concludes the first series of demos.
+
+Now that the first (few) pitfall(s) have been exposed, and the primary
+motivations have been demonstrated, it's time to dig into more corner cases and maybe even show off a bit.
+EOM
